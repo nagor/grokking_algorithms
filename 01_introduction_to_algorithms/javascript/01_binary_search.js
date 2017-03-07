94f5e055f5c210598cf2b342d7adddc9c256739b
Roman Nagorkin <roman.nagorkin@gmail.com>
2017-03-07 18:10:55 -0500

    Added mocha tests; wrapped binary search to module

--- 01_introduction_to_algorithms/javascript/01_binary_search.js
+++ 01_introduction_to_algorithms/javascript/01_binary_search.js
@@ -1,9 +1,10 @@
+(function (exports) {
 'use strict';
 
 function binary_search(list, item) {
   let low = 0;
   let high = list.length - 1;
-  
+
   while (low <= high) {
     let mid = Math.floor((low + high) / 2);
     let guess = list[mid];
@@ -16,11 +17,13 @@ function binary_search(list, item) {
       low = mid + 1;
     }
   }
-  
+
   return null;
 }
 
-const my_list = [1, 3, 5, 7, 9];
-
-console.log(binary_search(my_list, 3)); // 1
-console.log(binary_search(my_list, -1)); // null
+exports.search = binary_search;
+}(
+  (typeof exports === 'undefined')
+      ? window
+      : exports
+));
---
+++ 01_introduction_to_algorithms/javascript/test.js
@@ -0,0 +1,23 @@
+describe('Binary search tests', () => {
+    const { search } = require('./01_binary_search');
+    const should = require('should');
+
+    it('should return position of the item in the array', () => {
+        search([1, 2, 5, 7, 9], 5)
+            .should.be.a.Number()
+            .and.be.exactly(2);
+    });
+
+    it('should return null if item not found', () => {
+        should(search([1, 2, 5, 7, 9], 13)).be.a.Null();
+    });
+
+    it('should work with boundary values', () => {
+        should(search([1, 2, 5, 7, 9], 1)).be.exactly(0);
+        should(search([1, 2, 5, 7, 9], 9)).be.exactly(4);
+    });
+
+    it('should return null if array is empty', () => {
+        should(search([], 1)).be.a.Null();
+    });
+});
