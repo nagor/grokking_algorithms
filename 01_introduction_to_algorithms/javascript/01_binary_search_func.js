f82cb10fadc6e370809896e3f206b90224b5eb8b
Roman Nagorkin <roman.nagorkin@gmail.com>
2017-03-08 14:13:22 -0500

    Lambdas

--- 01_introduction_to_algorithms/javascript/01_binary_search_func.js
+++ 01_introduction_to_algorithms/javascript/01_binary_search_func.js
@@ -1,28 +1,27 @@
 (function (exports) {
-'use strict';
-
-function binary_search(list) {
-  return function (item) {
-    function search(list, item, low = 0, high = 0) {
-      const boundary = low >= high;
-      const mid = Math.floor((low + high) / 2);
-      const guess = list[mid];
-      const match = guess === item;
-      return match || boundary
-        ? match
-          ? mid
-          : null
-        : guess > item
-          ? search(list, item, low, mid - 1)
-          : search(list, item, mid + 1, high);
+    'use strict';
+    const binarySearch = list => item => {
+        const search = (list, item) => (low = 0, high = 0) => {
+            const boundary = low >= high;
+            const mid = Math.floor((low + high) / 2);
+            const guess = list[mid];
+            const match = guess === item;
+            const searchInList = search(list, item);
+            return match || boundary
+                ? match
+                    ? mid
+                    : null
+                : guess > item
+                    ? searchInList(low, mid - 1)
+                    : searchInList(mid + 1, high);
+        };
+        return search(list, item)(0, list.length - 1);
     }
-    return search(list, item, 0, list.length - 1);
-  };
-}
 
-exports.search = binary_search;
+    exports.search = binarySearch;
 }(
-  (typeof exports === 'undefined')
-      ? window
-      : exports
-));
+    (typeof exports === 'undefined')
+        ? window
+        : exports
+    )
+);
